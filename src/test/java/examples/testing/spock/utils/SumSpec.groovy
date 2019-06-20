af4332c5949adad7b270149b2e528e84aee18353
pawel.tarkowski <pawel.tarkowski@gmail.com>
2019-06-20 18:07:19 +0200

    #15 add spock example

---
+++ src/test/java/examples/testing/spock/utils/SumSpec.groovy
@@ -0,0 +1,21 @@
+package examples.testing.spock.utils
+
+import examples.testing.utils.Sum
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class SumSpec extends Specification {
+
+    @Unroll
+    def "should sum all elements #description"() {
+        when:
+        def result = operation.calculate()
+        then:
+        Double.compare(result, expected) == 0
+        where:
+        operation      | expected | description
+        new Sum(1, 2)  | 3        | "1 + 2 = 3"
+        new Sum(0, 1)  | 1        | "0 + 1 = 1"
+        new Sum(-1, 2) | 1        | "-1 + 2  = 1"
+    }
+}
