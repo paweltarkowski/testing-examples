1bcddb1b82257031b3f5aff90a6b79b45c1e74fe
pawel.tarkowski <pawel.tarkowski@gmail.com>
2019-06-20 18:08:35 +0200

    #0 set public for test class

--- src/test/java/examples/testing/junit5/utils/SumTest.java
+++ src/test/java/examples/testing/junit5/utils/SumTest.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 @DisplayName("Sum Assertions")
-class SumTest {
+public class SumTest {
 
     static Stream<Arguments> forShouldAddNumbers() {
         return of(
