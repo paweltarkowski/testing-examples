bbaac2b68abbd172fc08fa80744553f814dc8190
Pawel Tarkowski <PTarkowski@moneygram.com>
2019-11-21 18:51:23 +0100

    #16 add more junits for calculator operations

--- src/test/java/examples/testing/spock/service/CalculatorSpec.groovy
+++ src/test/java/examples/testing/spock/service/CalculatorSpec.groovy
@@ -11,24 +11,51 @@ class CalculatorSpec extends Specification {
     private Calculator testObject = new Calculator()
 
     @Unroll
-    def "should sum  #firstNumber + #secondNumber = #result"() {
+    def "should sum  #firstNumber + #secondNumber = #expected"() {
         expect:
-        testObject.sum(firstNumber, secondNumber) == result
+        testObject.sum(firstNumber, secondNumber) == expected
 
         where:
-        firstNumber | secondNumber || result
+        firstNumber | secondNumber || expected
         1           | 2            || 3
         1.5         | 2.8          || 4.3
+        1           | -2           || -1
     }
 
     @Unroll
-    def "should multiply #firstNumber * #secondNumber = #result"() {
+    def "should multiply #firstNumber * #secondNumber = #expected"() {
         expect:
-        testObject.multiply(firstNumber, secondNumber) == result
+        testObject.multiply(firstNumber, secondNumber) == expected
 
         where:
-        firstNumber | secondNumber || result
+        firstNumber | secondNumber || expected
         1           | 2            || 2
         1.5         | 3.02         || 4.53
+        0           | 1            || 0
+    }
+
+    @Unroll
+    def "should divide #firstNumber/#secondNumber = #expected"() {
+        expect:
+        testObject.divide(firstNumber, secondNumber) == expected
+
+        where:
+        firstNumber | secondNumber || expected
+        1           | 2            || 0.5
+        2           | 1            || 2
+        0           | 1            || 0
+    }
+
+    @Unroll
+    def "should throw exception because of #someNumber is division by 0"() {
+        when:
+        testObject.divide(someNumber, 0)
+
+        then:
+        def exc = thrown(ArithmeticException)
+        exc.getMessage().contains(someNumber.toPlainString())
+
+        where:
+        someNumber << [0.0, 1.0, 2.0]
     }
 }
