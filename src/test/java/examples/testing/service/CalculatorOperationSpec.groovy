b7c24d71ed1f930ec59059c2017c64cf09248930
Paweł Tarkowski <paweltarkowski@users.noreply.github.com>
2020-07-17 20:56:23 +0200

    #28 add example to verify invocation count and result (#35)
    Co-authored-by: Pawel Tarkowski

--- src/test/java/examples/testing/service/CalculatorOperationSpec.groovy
+++ src/test/java/examples/testing/service/CalculatorOperationSpec.groovy
@@ -8,27 +8,46 @@ import spock.lang.Unroll
 
 class CalculatorOperationSpec extends Specification {
 
+    def SOME_VALUE = 1
+
     @Shared
     private Calculator testObject = new Calculator()
 
-    @Unroll
-    def "calculator should return value calculated by operation: #value"() {
+    def "calculator should check if method operation return proper value "() {
         given:
+        //Stub is used because we do not need check of invocation count and verify only result of the method
         Operation operation = Stub()
-        operation.calculate() >> value
+        //we can declare mocking method in "given: because we do not check invocation count of this method in "then" block
+        operation.calculate() >> SOME_VALUE
 
         expect:
-        testObject.calculate(operation) == value
+        testObject.calculate(operation) == SOME_VALUE
+    }
+
+    @Unroll
+    def "calculator should invoke operation method and return: #expected"() {
+        given:
+
+        Operation operation = Mock()
+        when:
+        def result = testObject.calculate(operation)
+
+        then:
+        //invocation count and checking results requires some kind of trick - return mock value is defined in "then" block
+        //instead of "given" block to allow spock to work as expected
+        1 * operation.calculate() >> expected
+        result == expected
+
 
         where:
-        value << [1, 3, 5]
+        expected << [1, 3, 5]
     }
 
     def "calculator throw exception thrown by operation"() {
         given:
         Operation operation = Stub()
         operation.calculate() >> {
-            throw new ArithmeticException("SOME EXCEPTION");
+            throw new ArithmeticException("SOME EXCEPTION")
         }
 
         when:
